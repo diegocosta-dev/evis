ccad0a04e25c012f03c5baaa79979469b90a1ec4
Diego Costa <troiablender@gmail.com>
2023-12-12 12:59:31 -0300

    change seo

--- components/Container.js
+++ components/Container.js
@@ -7,8 +7,8 @@ export default function Container(props) {
   const { children, ...customMeta } = props
   const router = useRouter()
   const meta = {
-    title: 'A Next.js, Notion, Tailwind CSS starter blog template',
-    description: `An open-source starter blog template that is statically generated with Next.js, content powered by Notion and styled with Tailwind CSS.`,
+    title: 'Evis template',
+    description: `Evis`,
     type: 'website',
     image: '/site.png',
     ...customMeta,
